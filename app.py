bb335b56a52ddbdfc6b53f653900a46d01d34804
Eric Itokazu <itokazu.eric@gmail.com>
2026-06-22 22:00:27 -0700

    Added practice throw tab with direct google sheet call. Create secret for strlit

--- app.py
+++ app.py
@@ -85,7 +85,10 @@ def load_practice_data():
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive",
     ]
-    creds = Credentials.from_service_account_file("throws-data-conn.json", scopes=scopes)
+    creds = Credentials.from_service_account_info(
+        dict(st.secrets["gcp_service_account"]),
+        scopes=scopes,
+    )
     client = gspread.authorize(creds)
     records = client.open("Throw Data Tracking").sheet1.get_all_records()
     df = pd.DataFrame(records)
